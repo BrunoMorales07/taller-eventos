800720ecae587d9ad0d84bed64a2f6deebe8a9ea
BrunoMorales07 <bruno.morales.ia2016@gmail.com>
2025-08-17 13:54:20 -0300

    Probe con stoppropagation

--- eventos.js
+++ eventos.js
@@ -1,4 +1,14 @@
-let btn = document.querySelector("#btn");
-btn.addEventListener("click", function() {
-    alert("Hola! Soy el div!");
+
+let Boton = document.getElementById('btn');
+let Div = document.getElementById('div');
+
+Boton.addEventListener('click', (evento) => {
+  evento.stopPropagation();
+  alert('Hola!');
+  console.log('Hiciste clic en el botón');
+});
+
+Div.addEventListener('click', (evento) => {
+  alert('Hola! Soy el div!');
+  console.log('Hiciste clic en el div');
 });
